40aed8dfc589deee6531a51d091fca092bf56985
Geoffrey Lalanne <geoffrey.lalanne@teck.com>
2025-08-22 15:47:32 -0700

    [Hack] Expose the tf provider for upjet

---
+++ xpprovider/xpprovider.go
@@ -0,0 +1,11 @@
+package xpprovider
+
+import (
+	"context"
+	"github.com/databricks/terraform-provider-databricks/internal/providers/sdkv2"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func GetProvider(_ context.Context) (*schema.Provider, error) {
+	return sdkv2.DatabricksProvider(), nil
+}
